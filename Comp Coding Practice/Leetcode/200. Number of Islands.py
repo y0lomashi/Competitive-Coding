5b7d59197f454e25c33b462306a6445e73accf6d
Curtis Li <curtisli7788@gmail.com>
2023-02-12 12:49:48 -0500

    leetcode and cheerio competition

---
+++ Comp Coding Practice/Leetcode/200. Number of Islands.py
@@ -0,0 +1,47 @@
+# Leetcode 200. Number of Islands
+# https://leetcode.com/problems/number-of-islands/
+
+from typing import List
+import collections
+
+
+class Solution:
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(r, c):
+            q = collections.deque()
+            visited.add((r, c))
+            q.append((r, c))
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r in range(rows) and c in range(cols)
+                            and grid[r][c] == "1" and (r, c) not in visited):
+                        q.append((r, c))
+                        visited.add((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    islands += 1
+                    bfs(r, c)
+
+        return islands
+
+
+# ! for testing
+grid = [["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"]
+        ]
+print(Solution().numIslands(grid))
