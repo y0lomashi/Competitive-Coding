6e19ee0800f6340695332ca06228e609c1f7e7a5
Curtis Li <curtisli7788@gmail.com>
2023-03-05 22:58:45 -0500

    cf problem

---
+++ Comp Coding Files/Codeforces/Codeforces Round 856 (Div. 2)/B. Not Dividing.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define SZ(x) int(x.size())
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sortAll(x) sort(all(x))
+#define PI 3.1415926535897932384626
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            if (a[i] == 1)
+            {
+                a[i]++;
+            }
+        }
+        
+        
+        for (int i = 1; i < sizeof(a)/sizeof(a[0]); i++) {
+            while (a[i] % a[i - 1] == 0) 
+            {
+                a[i]++;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
